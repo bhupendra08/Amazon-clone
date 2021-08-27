e48c2915165efb51894a892058d09031caaab363
Bhupendra Jeena <bhupendrasinghjeena84@gmail.com>
2021-08-26 17:41:26 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div>
+      <h1> Amazon clone</h1>
+      <input type="text"/>
+      <img src="./images/logo.png" alt="logo"/>
+      <Header />
+      {/*Home*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,47 @@
+import React from 'react';
+const Header=()=>{
+    return(
+        <>
+        <h1>Header</h1>
+        <img src="./images/logo.png"/>
+        <input type="text"/>
+        </>
+    )
+}
+
+
+/*function Header() {
+    return (
+        <div>
+            <img
+                
+                src="./images/logo.png"/>
+            
+
+            <div className="header_seacrh">
+                <input
+                    className="header_input_search"
+                    type='text' />
+            </div>
+
+            <div className="header_nav">
+                <div className="header_nav_option">
+                    <span className="line1">Hello</span>
+                    <span className="line2">Sign In</span>
+                </div>
+                <div className="header_nav_option">
+                    <span className="line1">Returns</span>
+                    <span className="line2">& Orders</span>
+                </div>
+                <div className="header_nav_option">
+                    <span className="line1">Your</span>
+                    <span className="line2">Prime</span>
+                </div>
+
+
+            </div>
+        </div>
+    )
+}*/
+
+export default Header;
